5074296f8a0bcea2fe4cdc74175ad9ae2cc7a44f
Michael Lackey <michael.g.lackey@gmail.com>
2020-03-04 14:58:53 -0600

    update book show page

--- models/user.js
+++ models/user.js
@@ -1,10 +1,20 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const ratingSchema = new Schema ({
+    rating: {
+        type: String,
+        enum: [1, 2, 3, 4, 5]
+    }
+}, {
+    timestamps: true
+});
+
 const bookSchema = new Schema ({
     title: String,
     author: String,
     publishYear: String,
+    rating: [ratingSchema]
 }, {
     timestamps: true
 });
@@ -14,7 +24,7 @@ const userSchema = new Schema ({
     email: String,
     avatar: String,
     googleId: String,
-    books: [bookSchema],
+    books: [bookSchema]
 }, {
     timestamps: true
 });
